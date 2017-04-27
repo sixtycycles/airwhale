5a7fb76fa65b6d2e023dadbda96eac1d3b44c754
seieibob <joncole04412@gmail.com>
2017-04-27 14:46:37 -0400

    Move to use partials

--- auth/fpass.php
+++ auth/fpass.php
@@ -24,17 +24,17 @@ if(isset($_POST['btn-submit']))
         $stmt->execute(array(":token"=>$code,"email"=>$email));
 
         $message= "
-       Hello , $email
-       <br /><br />
-       Someone has requested a password reset for this your account. if that was you, great!<br />
-       Click Following Link To Reset Your Password 
-       <br /><br />
-       <a href='localhost/auth/resetpass.php?id=$id&code=$code'>click here to reset your password</a>
-       <br /><br />
-       if that wasnt you, ignore this email. 
-       thank you,
-       Team Leader
-       ";
+        Hello , $email
+        <br /><br />
+        Someone has requested a password reset for this your account. if that was you, great!<br />
+        Click Following Link To Reset Your Password 
+        <br /><br />
+        <a href='localhost/auth/resetpass.php?id=$id&code=$code'>click here to reset your password</a>
+        <br /><br />
+        if that wasnt you, ignore this email. 
+        thank you,
+        Team Leader
+        ";
         $subject = "Password Reset";
 
         $user->send_mail($email,$message,$subject);
@@ -58,50 +58,16 @@ if(isset($_POST['btn-submit']))
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Import common header -->
+    <?php require_once "../partials/head.php"; ?>
     <title>Reset Password | Orono Problem Reporter</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-            crossorigin="anonymous"></script>
 </head>
+
 <body id="login">
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Town Of Orono Issue Tracker</a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+<!-- Import navbar -->
+<?php require_once "../partials/navbar.php"; ?>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">Menu <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li> <a href="home.php">Report Problems</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="logout.php">logout</a></li>
-                    </ul>
-                </li>
-
-            </ul>
-
-
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
 <div class="container">
 
     <form class="form-signin" method="post">
--- auth/verify.php
+++ auth/verify.php
@@ -59,50 +59,16 @@ if(isset($_GET['id']) && isset($_GET['code']))
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Import common header -->
+    <?php require_once "../partials/head.php"; ?>
     <title>Confirm Registration</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-            crossorigin="anonymous"></script>
 </head>
+
 <body id="login">
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Town Of Orono Issue Tracker</a>
-        </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+<!-- Import navbar -->
+<?php require_once "../partials/navbar.php"; ?>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false">Menu <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li> <a href="home.php">Report Problems</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="logout.php">logout</a></li>
-                    </ul>
-                </li>
-
-            </ul>
-
-
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
 <div class="container">
 
     <?php if(isset($msg)) { echo $msg; } ?>
