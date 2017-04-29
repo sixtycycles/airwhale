3c32237f06ad50160a7eda3b558a39215ce48570
Rod <sixtycycles@gmail.com>
2017-04-29 12:30:50 -0400

    reworked download query to be explicit for values.
    (this lets us download the problem names instead of id's for export.
    
    I also decided that explicitly stating the columns form the db rather than using a wildcard is better in this instance, (esp since the join adds lots of duped columns.

--- auth/download.php
+++ auth/download.php
@@ -10,13 +10,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql2 = "SELECT * FROM Problems";
+$sql2 = "SELECT id, name, lat, lon, description,timestamp,problem_status, file,tbl_problem_types.type_name 
+FROM Problems 
+  Inner JOIN tbl_problem_types 
+    ON (Problems.type_id=tbl_problem_types.type_id);";
 
 $result = mysqli_query($conn, $sql2) or die ("Selection Error " . mysqli_error($conn));
 
 $fp = fopen('php://output', 'w');
 
-$headings = ['PROBLEM_ID', 'USERNAME', 'LATTITUDE', 'LONGITUDE', 'DESCRIPTION', 'TYPE', 'SUBMIT_DATETIME', 'STATUS', 'IMAGE_NAME'];
+$headings = ['PROBLEM_ID', 'USERNAME', 'LATTITUDE', 'LONGITUDE', 'DESCRIPTION','SUBMIT_DATETIME', 'STATUS', 'IMAGE_NAME','PROBLEM_TYPE'];
 
 fputcsv($fp, $headings);
 
