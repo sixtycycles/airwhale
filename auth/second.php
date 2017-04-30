259b974932c7dd59d606c9c01f2bfc7072d11e7b
Rod <sixtycycles@gmail.com>
2017-04-30 08:27:35 -0400

    non multiple liking kinda works?
    there may be an issue with how the user is checked. also other users may not be able to like after you like, which is an issue.

--- auth/second.php
+++ auth/second.php
@@ -1,38 +1,52 @@
 <?php
 //header('Location: home.php');
+session_start();
 require_once('phpsqlinfo_dbinfo.php');
 
 if (!empty($_GET)) {
 
+    $problems_id = htmlspecialchars($_GET['id']);
+    //var_dump($_SESSION['userSession']);
+    $uID =  $_SESSION['userSession'];
+    //echo $uID;
+    $connection = mysqli_connect("localhost", $username, $password, $database, 8889);
 
-    $id = htmlspecialchars($_GET['id']);
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+    $selectQuery = "SELECT likes from Problems where id = $problems_id";
+    $result = mysqli_query($connection, $selectQuery);
+
+    $row = mysqli_fetch_assoc($result);
+
+    $likes = $row['likes'] + 1;
 
 
+    //set likes to existing likes plus 1
+    if (!empty($row)) {
 
-        $connection = mysqli_connect("localhost", $username, $password, $database, 8889);
+        //check if user has liked already
+        $haslikedQuery = "Select *
+                              from likes
+                              where user = $uID AND problem_id = $problems_id;";
+        $resultLikes = mysqli_query($connection, $haslikedQuery);
 
-        if ($connection->connect_error) {
-            die("Connection failed: " . $connection->connect_error);
-        }
-        $selectQuery = "SELECT likes from Problems where id = $id";
-        $result = mysqli_query($connection, $selectQuery);
+        $likeRow = mysqli_fetch_assoc($resultLikes);
 
-        $row = mysqli_fetch_assoc($result);
-
-        $likes = $row['likes'] + 1;
-
-        //set likes to existing likes plus 1
-        if (!empty($row)) {
-
-            //TODO add tvals to likes table
-            $updateQuery = "UPDATE Problems SET likes = $likes WHERE id = $id";
+        if (empty($likeRow)) {
+            $updateQuery = "UPDATE Problems SET likes = $likes WHERE id = $problems_id";
             $result2 = mysqli_query($connection, $updateQuery);
+
+            $updateLikes = "INSERT INTO likes (user, problem_id ) VALUES( $uID, $problems_id )";
+            $res = mysqli_query($connection, $updateLikes);
+            echo $likes;
+        } else {
+            echo "already liked";
         }
-        echo "$likes";
-        //var_dump($_SESSION);
+    }
 
-        $connection->close();
-
-
-    exit();
+    $connection->close();
 }
+
+
+
