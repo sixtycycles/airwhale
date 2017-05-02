f109900fa75b8b7c67ca70370c6a48aca203e9e3
seieibob <joncole04412@gmail.com>
2017-05-02 13:42:04 -0400

    Edit formatting and styling

--- page/index.php
+++ page/index.php
@@ -39,12 +39,11 @@ if(isset($_POST['btn-login']))
     if(isset($_GET['inactive']))
     {
         ?>
-        <div class='alert alert-error'>
-            <button class='close' data-dismiss='alert'>&times;</button>
-            <strong>Sorry!</strong> This account is not activated! Go to your inbox and activate it.
+        <div class='alert alert-danger'>
+            This account is not activated! Please check your inbox for an email about activating your account.
         </div>
         <?php
-    }
+    };
     ?>
     <div class="row">
         <div class="col-lg-6 col-md-6">
@@ -53,14 +52,16 @@ if(isset($_POST['btn-login']))
                 if(isset($_GET['error']))
                 {
                     ?>
-                    <div class='alert alert-success'>
-                        <button class='close' data-dismiss='alert'>&times;</button>
+                    <div class='alert alert-danger'>
                         <strong>Sorry, that doesn't seem to be correct! Did you mistype something?</strong>
-                    </div>
+                    </div>;
                     <?php
-                }
+                };
                 ?>
-                <h2 class="form-signin-heading">Please log in to report problems.</h2><hr />
+                <h2 class="form-signin-heading">Please log in to report problems.</h2>
+
+                <hr />
+
                 <div class="form-group">
                     <label for="txtemail">Email Address</label>
                     <input type="email" class="input-block-level form-control" placeholder="Email address" name="txtemail" required />
@@ -69,10 +70,13 @@ if(isset($_POST['btn-login']))
                     <label for="txtupass">Password</label>
                     <input type="password" class="input-block-level form-control" placeholder="Password" name="txtupass" required />
                 </div>
+
                 <hr />
+
                 <button class="btn btn-large btn-primary" type="submit" name="btn-login">Log In</button>
                 <a href="signup.php" class="btn btn-large">Sign Up</a><hr />
                 <a href="fpass.php">Lost your Password?</a>
+
             </form>
         </div>
     </div>
